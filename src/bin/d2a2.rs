c5657c42f224d69d6471528fb5a5f9e40cd545d4
Joona Luoma <joona.luoma@hotmail.com>
2025-04-16 21:25:39 +0300

    tidy day 2 up

--- src/bin/d2a2.rs
+++ src/bin/d2a2.rs
@@ -3,72 +3,72 @@ use std::fs;
 
 fn main() -> Result<()> {
     let input = fs::read_to_string("src/bin/d2a1/input")?;
-    let mut records = Vec::<Vec<i32>>::new();
-    for line in input.lines() {
-        let mut levels = Vec::<i32>::new();
-        for level in line.split_ascii_whitespace().map(|v| v.parse::<i32>().context("Bad number.")).collect::<Result<Vec<_>>>()? {
-            levels.push(level);
-        }
-        records.push(levels);
-    }
 
-    let mut safe = 0;
-    'record: for record in records {
-        if let Some(uix) = unsafe_ix(record.iter()) {
-            'check: {
-                if unsafe_ix(without_ix(&record, uix)) == None {
-                    break 'check
-                } else if uix > 0 && unsafe_ix(without_ix(&record, uix - 1)) == None {
-                    break 'check
-                } else if uix < record.len() && unsafe_ix(without_ix(&record, uix + 1)) == None {
-                    break 'check
-                }
-                continue 'record
-            }
-        }
+    let records: Vec<Vec<i32>> = input
+        .lines()
+        .map(|line| line
+            .split_ascii_whitespace()
+            .map(|v| v
+                .parse::<i32>()
+                .with_context(|| format!("Bad number: {v}"))
+            )
+            .collect::<Result<_>>()
+        )
+        .collect::<Result<_>>()?;
 
-        safe += 1;
-    }
+    let safe = records
+        .iter()
+        .filter(|r| is_safe(r))
+        .count();
 
     fs::write("src/bin/d2a2/output", format!("{safe}"))?;
 
     Ok(())
 }
 
+fn is_safe(record: &[i32]) -> bool {
+    if let Some(uix) = unsafe_ix(record.iter()) {
+        for cix in [uix.wrapping_sub(1), uix, uix + 1] {
+            if unsafe_ix(without_ix(record, cix)).is_none() {
+                return true
+            }
+        }
+        return false
+    }
+    true
+}
+
 fn without_ix<T>(v: &[T], ix: usize) -> impl Iterator<Item = &T> {
-    v[..ix].iter().chain(&v[ix+1..])
+    v.iter().enumerate().filter(move |(i, _)| *i != ix).map(|(_, v)| v)
 }
 
 fn unsafe_ix<'a>(iter: impl Iterator<Item = &'a i32>) -> Option<usize> {
+    
     #[derive(PartialEq)]
     enum Dir { Incr, Decr, }
+    
     let mut dir: Option<Dir> = None;
-    let mut first = true;
-    let mut l_ix: usize = 0;
-    let mut val_prev = 0;
-    for val in iter {
-        let diff = val_prev - val;
-        val_prev = *val;
+    let mut prev: Option<i32> = None;
 
-        if first {
-            first = false;
-            continue
+    for (ix, &val) in iter.enumerate() {
+        if let Some(prev) = prev {
+            let ix = ix - 1;
+            let diff = val - prev;
+
+            let dir_curr = match diff {
+                v if v.abs() > 3 => return Some(ix),
+                v if v > 0 => Dir::Incr,
+                v if v < 0 => Dir::Decr,
+                _ => return Some(ix),
+            };
+
+            dir = match dir {
+                None => Some(dir_curr),
+                Some(dir) if dir != dir_curr => return Some(ix),
+                _ => dir,
+            };
         }
-
-        let dir_curr = match diff {
-            v if v.abs() > 3 => return Some(l_ix),
-            v if v > 0 => Dir::Incr,
-            v if v < 0 => Dir::Decr,
-            _ => return Some(l_ix),
-        };
-
-        dir = match dir {
-            None => Some(dir_curr),
-            Some(v) if v != dir_curr => return Some(l_ix),
-            _ => dir
-        };
-
-        l_ix += 1;
+        prev = Some(val);
     }
     None
 }
